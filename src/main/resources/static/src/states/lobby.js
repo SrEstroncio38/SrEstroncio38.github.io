81a6b8fe72214ac1b683a26dc4f1122b0e947303
David Fontela <d.fontela.1998@gmail.com>
2019-05-20 20:37:27 +0200

    Imagenes Lobby

--- src/main/resources/static/src/states/lobby.js
+++ src/main/resources/static/src/states/lobby.js
@@ -1,24 +1,54 @@
 Spacewar.lobbyState = function(game) {
-	
+
+
+
+
+}
+
+function actionOnClick(){
+
+    lobby.visible.setTo = false;
+    console.log("hola");
 }
 
 Spacewar.lobbyState.prototype = {
 
-	init : function() {
-		if (game.global.DEBUG_MODE) {
-			console.log("[DEBUG] Entering **LOBBY** state");
-		}
-	},
+    init : function() {
+        if (game.global.DEBUG_MODE) {
+            console.log("[DEBUG] Entering LOBBY state");
+        }
 
-	preload : function() {
-		
-	},
+        //game.world.setBounds(0, 0, 1920, 1920);
+    },
 
-	create : function() {
-		game.state.start('matchmakingState');
-	},
+    preload : function() {
+        game.load.image('lobby','assets/images/lobby.png');
+        game.load.image('+','assets/buttons/Play_BTN.png');
+        game.load.image('-','assets/buttons/Close_BTN.png');
+        game.load.image('+A','assets/buttons/Play_BTN_act.png');
+        game.load.image('-A','assets/buttons/Close_BTN_act.png');
 
-	update : function() {
-		
-	}
+    },
+
+    create : function() {
+
+        lobby = game.add.sprite(game.world.centerX, game.world.centerY, 'lobby');
+        lobby.scale.setTo(0.8,0.8);
+        lobby.anchor.setTo(0.5,0.5);
+        addRoom = game.add.button(game.world.centerX + 240,game.world.centerY + 180, '+', actionOnClick, this, '+A', '+','+A', '+');
+        addRoom.scale.setTo(0.3, 0.3);
+        close = game.add.button(game.world.centerX + 170,game.world.centerY + 180, '-', actionOnClick, this, 2, 1, 0);
+        close.scale.setTo(0.3, 0.3);
+
+
+    },
+
+    update : function() {
+
+
+    },
+
+
+
+
 }
